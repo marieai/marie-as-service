0cfa4c13587d7bea33eeb43a149b3a5f37b61f79
gbugaj <gbugaj@rmsweb.com>
2023-02-22 18:05:41 -0600

    chore: s3 storage integration

--- server/marie_server/__main__.py
+++ server/marie_server/__main__.py
@@ -11,6 +11,7 @@ from marie.messaging import (
     AmazonMQToastHandler,
     PsqlToastHandler,
 )
+from marie.storage import S3StorageHandler, StorageManager
 from marie.utils.device import gpu_device_count
 
 from marie.constants import (
@@ -24,7 +25,6 @@ from marie_server.rest_extension import extend_rest_interface
 
 
 def setup_toast_events(toast_config: Dict[str, Any]):
-
     print(toast_config)
     native_config = toast_config["native"]
     psql_config = toast_config["psql"]
@@ -33,6 +33,18 @@ def setup_toast_events(toast_config: Dict[str, Any]):
     Toast.register(NativeToastHandler("/tmp/events.json"), native=True)
     Toast.register(PsqlToastHandler(psql_config), native=False)
     Toast.register(AmazonMQToastHandler(amazon_config), native=False)
+
+
+def setup_storage(storage_config: Dict[str, Any]):
+    """Setup the storage handler"""
+    print(storage_config)
+
+    if "s3" in storage_config:
+        handler = S3StorageHandler(config=storage_config["s3"], prefix="S3_")
+
+        # export AWS_ACCESS_KEY_ID=MARIEACCESSKEY; export AWS_SECRET_ACCESS_KEY=MARIESECRETACCESSKEY;  aws s3 ls --endpoint-url http://localhost:8000
+        StorageManager.register_handler(handler=handler)
+        StorageManager.ensure_connection("s3://")
 
 
 if __name__ == "__main__":
@@ -57,14 +69,11 @@ if __name__ == "__main__":
     else:
         _input = "torch-flow.yml"
 
-    _config_file = "marie.yml"
-
     print(f"__model_path__ = {__model_path__}")
     print(f"__config_dir__ = {__config_dir__}")
     print(f"__marie_home__ = {__marie_home__}")
     print(f"__cache_path__ = {__cache_path__}")
     print(f"_input = {_input}")
-    print(f"_config_file = {_config_file}")
     print(f"CONTEXT.gpu_device_count = {gpu_device_count()}")
 
     # Load the config file and setup the toast events
@@ -77,10 +86,9 @@ if __name__ == "__main__":
     )
 
     print(config)
-    if "toast" not in config:
-        config["toast"] = {}
 
-    setup_toast_events(config["toast"])
+    setup_toast_events(config.get("toast", {}))
+    setup_storage(config.get("storage", {}))
 
     os.exit()
     f = Flow.load_config(
